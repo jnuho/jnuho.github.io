af11931fbdff27676271d89799b14f8bd1f530e9
junho-rowem <jnuho@rowem.com>
2021-09-08 01:58:59 +0900

    .

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-import requests
+N, K = map(int, input().split())
 
-str = "abc"
-print(f"value = {str}")
+count=0
+while N > 1:
+  if N % K == 0:
+    N = N/K
+  else:
+    N= N-1
+  count+=1
+
+print(count)
