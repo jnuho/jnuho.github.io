bbf4228ec52be9c4e3ef74d15140b1190c0e5b51
junho-rowem <jnuho@rowem.com>
2021-09-09 16:59:58 +0900

    .

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-N, K = map(int, input().split())
-
-count=0
-while N > 1:
-  if N % K == 0:
-    N = N/K
-  else:
-    N= N-1
-  count+=1
+# 00:00:00 ~ N:59:59
+# 3포함 개수
+N = int(input())
+count = 0
+for h in range(0, N+1):
+  for m in range(0, 60):
+    for s in range(0, 60):
+      if '3' in str(h) + str(m) + str(s):
+        count+=1
 
 print(count)
