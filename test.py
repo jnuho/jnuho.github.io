f273cf88631cc26a4640fb7978379eff04b4119d
junho-rowem <jnuho@rowem.com>
2021-09-26 02:01:53 +0900

    .

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import sys
 
-# 하나의 문자열 데이터 입력
-input_data = sys.stdin.readline().rstrip()
+N = int(sys.stdin.readline().rstrip())
+arr1 = list(map(int, sys.stdin.readline().rstrip().split()))
+arr1.sort()
 
-# 입력 받은 문자열 그대로 출력
-print(input_data)
+M = int(sys.stdin.readline().rstrip())
+arr2 = list(map(int, sys.stdin.readline().rstrip().split()))
+
+def binary_search(array, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+
+    if target == arr1[mid]:
+      return mid
+    elif target > arr1[mid]:
+      start=mid+1
+    else:
+      end = mid-1
+  return None
+
+for i in arr2:
+  result = binary_search(arr1, i, 0, N-1)
+  if result != None:
+    print("yes", end= ' ')
+  else:
+    print("no", end= ' ')
