d1bcb01b551536dfbefcb24a06c6c7091f46745b
junho-rowem <jnuho@rowem.com>
2021-09-27 00:33:57 +0900

    .

---
+++ aws.md
@@ -0,0 +1,16 @@
+
+```sh
+sudo yum repolist
+sudo yum install epel-release
+sudo yum list installed | grep epel
+```
+
+- Nodejs
+
+```sh
+# nvm 설치
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+~/.nvm/nvm.sh
+
+# Nodejs 설치
+```
--- test.py
+++ test.py
@@ -1,27 +1,8 @@
-import sys
+from typing import List
 
-N = int(sys.stdin.readline().rstrip())
-arr1 = list(map(int, sys.stdin.readline().rstrip().split()))
-arr1.sort()
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numToIdx = {}
 
-M = int(sys.stdin.readline().rstrip())
-arr2 = list(map(int, sys.stdin.readline().rstrip().split()))
-
-def binary_search(array, target, start, end):
-  while start <= end:
-    mid = (start + end) // 2
-
-    if target == arr1[mid]:
-      return mid
-    elif target > arr1[mid]:
-      start=mid+1
-    else:
-      end = mid-1
-  return None
-
-for i in arr2:
-  result = binary_search(arr1, i, 0, N-1)
-  if result != None:
-    print("yes", end= ' ')
-  else:
-    print("no", end= ' ')
+    for i,n in enumerate(nums):
+      numToIdx[n] = i
