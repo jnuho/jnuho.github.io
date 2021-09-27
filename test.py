931d30411751fa07cb96e019e77c5241a6619477
jnuho <jnuho@outlook.com>
2021-09-27 17:09:29 +0900

    .

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 from typing import List
 
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    numToIdx = {}
 
-    for i,n in enumerate(nums):
-      numToIdx[n] = i
+  # Given a string s, find the length of the longest substring without repeating characters.
+  # Example 1:
+  # Input: s = "abcabcbb"
+  # Output: 3
+  # Explanation: The answer is "abc", with the length of 3.
+
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    dict = {}
+    dict[3] = "abc"
+
+    for i, c in enumerate(s):
+      print(s[i], end= ' ')
+    return 0
+
+result = Solution().lengthOfLongestSubstring("xddabcd")
