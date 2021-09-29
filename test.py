a6bbf53794b916e97583fe21b1d75398c353a659
junho-rowem <jnuho@rowem.com>
2021-09-29 18:07:26 +0900

    .

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
-from typing import List
-
 class Solution:
 
-  # Given a string s, find the length of the longest substring without repeating characters.
-  # Example 1:
-  # Input: s = "abcabcbb"
-  # Output: 3
-  # Explanation: The answer is "abc", with the length of 3.
+  # Given a string s, return the longest palindromic substring in s.
+  # Input: s = "babad"
+  # Output: "bab"
+  # Note: "aba" is also a valid answer.
+  def longestPalindrome(self, s: str) -> str:
+    longest = ""
+    for i,c in enumerate(s):
+      s_len = min(i, len(s)-i-1)
+      for j in range(s_len+1):
+        target = s[i-j:i+j+1]
+        if self.isPalindrome(target):
+          if len(target) > len(longest):
+            longest = target
+        target = s[i-j:i+j]
+        if self.isPalindrome(target):
+          if len(target) > len(longest):
+            longest = target
+    return longest
 
-  def lengthOfLongestSubstring(self, s: str) -> int:
-    dict = {}
-    dict[3] = "abc"
 
-    for i, c in enumerate(s):
-      print(s[i], end= ' ')
-    return 0
+    longest = ""
+    return longest
 
-result = Solution().lengthOfLongestSubstring("xddabcd")
+  def isPalindrome(self, s:str) -> str:
+    for i in range(len(s)//2):
+      if s[i] != s[len(s)-i-1]:
+        return False
+    return True
+
+result = Solution().longestPalindrome("cbbd")
+print(result)
