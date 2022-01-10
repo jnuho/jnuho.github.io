9d86213def19b56ede5824996f43565758c3d716
junho-rowem <jnuho@rowem.com>
2022-01-11 08:52:34 +0900

    .

--- IdeaProjects/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ IdeaProjects/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -10,11 +10,21 @@ import java.util.Date;
 //@Table(UniqueConstraint = )
 //@Entity(name="Member")
 //@Table(name="USER") // 테이블명 명시
+@SequenceGenerator(
+    name="member_seq_generator",
+    sequenceName = " member_seq", // 매핑할 데이터베이스 시퀀스 이름
+    initialValue=1, allocationSize = 1) // 성능최적화 파라미터
+//@TableGenerator(
+//    name="member_seq_generator",
+//    table = " my_sequences",
+//    pkColumnValue="member_seq", allocationSize = 1)
 public class Member {
 
   @Id
 //  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+// DB타입별 오라클 시퀀스,  MySQL auto_increment 대신 별도 테이블로 관리 (성능문제있음)
+//  @GeneratedValue(strategy = GenerationType.TABLE, generator="member_seq_generator)
   private Long id;
 
   @Column(name="name", nullable= false)
