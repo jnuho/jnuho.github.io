bef833c2c85bc9b6f9be15f10b18da55b04543bf
junho-rowem <jnuho@rowem.com>
2022-01-10 10:28:55 +0900

    .

--- test.py
+++ test.py
@@ -22,22 +22,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
 # N = 배열의 크기
 # M = 숫자가 더해지는 횟수
 # K = 연속으로 더할 수 있는 횟수
 # 5 8 3
 # 2 4 5 4 6
-N, M, K = map(int, input().split())
 
-list = list(map(int, input().split()))
-sum = 0
 
+N,M,K = 
