32cdf32db190c19e8752cf6ae677b06aa2cec713
junho-rowem <jnuho@rowem.com>
2022-01-11 17:25:37 +0900

    .

--- test.py
+++ test.py
@@ -5,20 +5,17 @@
 
 
 
-# top left (1,1) bottom right (N,N)
-# 계획서 띄어쓰기 기준 L R U D 문자들이 반복적으로 적혀있음
-# 움직일수 없는 곳 이동명령은 무시 됨
-# 5
-# R R R U D D
-# 3 4
-move_type = ['L', 'R', 'U', 'D']
 
-# LRUD
-r_moves = [0, 0, 1, -1]
 
-# 행열
-c_moves = [-1, 1, 0, 0]
-r, c = 1,1
-n = int(input())
-moves = list(input.split()) # LRUD 방향 리스트
 
+
+
+
+
+
+
+
+
+
+# 체스판에서 (a~h, 1~8)
+# 나이트 위치가 주어졌을때, 이동 가능한 경우의 수
