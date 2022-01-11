2b381ac30a55fc46773357ebaaddf07f91b90238
junho-rowem <jnuho@rowem.com>
2022-01-12 08:49:13 +0900

    .

--- IdeaProjects/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ IdeaProjects/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,7 @@ public class JpaMain {
       Member member = new Member();
       member.setUsername("C");
       em.persist(member);
+      System.out.println("member.id " + member.getId());
 
       tx.commit();
 
