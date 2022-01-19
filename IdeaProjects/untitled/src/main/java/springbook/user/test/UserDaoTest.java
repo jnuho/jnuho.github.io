6474fdbb8a1840314a89ff5bba0e6af28a3dc263
junho-rowem <jnuho@rowem.com>
2022-01-19 17:08:57 +0900

    .

--- IdeaProjects/untitled/src/main/java/springbook/user/test/UserDaoTest.java
+++ IdeaProjects/untitled/src/main/java/springbook/user/test/UserDaoTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
+import javax.management.InvalidApplicationException;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
--- test.py
+++ test.py
@@ -17,19 +17,5 @@
 
 
 
-# 체스판에서 (a~h, 1~8)
-# 나이트 위치가 주어졌을때, 이동 가능한 경우의 수
 
-user_input = input()
-x, y  = int(ord(user_input[0])) - 96, int(user_input[1])
-moves = [(2,1), (2,-1), (1,2), (-1,2), (-2,1), (-2,-1), (1,-2), (-1,-2)]
 
-count = 0
-for m in moves:
-  new_x = m[0] + x
-  new_y = m[1] + y
-
-  if new_x >= 1 and new_x <= 8 and new_y >= 1 and new_y <=8:
-    count += 1
-
-print(count)
