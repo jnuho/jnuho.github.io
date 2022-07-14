f3b37a0b17221b8b1736625d6dd0d57264cca426
jnuho <jnuho@outlook.com>
2022-07-14 22:47:06 +0900

    .

--- hellogo/26-search-word-project/ex26.3/ex26.3.go
+++ hellogo/26-search-word-project/ex26.3/ex26.3.go
@@ -1,30 +1,50 @@
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
-	"bufio"
 	"strings"
 )
 
-type LineInfo struct {
+// 찾은 라인 정보
+type LineInfo struct { // ❶ 찾은 결과 정보
 	lineNo int
-	line string
+	line   string
 }
 
+// 파일 내 라인 정보
 type FindInfo struct {
 	filename string
-	lines []LineInfo
+	lines    []LineInfo
 }
 
-/**
-cd 26-search-word-project/ex26.3
-go mod init 26-search-word-project/ex26.3
-go mod tidy
-go build
-./ex26.3 word ex*
-*/
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행 인수가 필요합니다. ex) ex26.3 word filepath")
+		return
+	}
+
+	word := os.Args[1] // ❷ 찾으려는 단어
+	files := os.Args[2:]
+	findInfos := []FindInfo{}
+	for _, path := range files {
+		// ❸ 파일 찾기
+		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, findInfo := range findInfos {
+		fmt.Println(findInfo.filename)
+		fmt.Println("--------------------------------")
+		for _, lineInfo := range findInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("--------------------------------")
+		fmt.Println()
+	}
+}
 
 func GetFileList(path string) ([]string, error) {
 	return filepath.Glob(path)
@@ -33,14 +53,14 @@ func GetFileList(path string) ([]string, error) {
 func FindWordInAllFiles(word, path string) []FindInfo {
 	findInfos := []FindInfo{}
 
-	filelist, err := GetFileList(path)
+	filelist, err := GetFileList(path) // ❶ 파일 리스트 가져오기
 	if err != nil {
 		fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path:", path)
 		return findInfos
 	}
 
-	for _, filename := range filelist {
-		findInfos = append(findInfos, FindWordInAllFiles(word, filename)...)
+	for _, filename := range filelist { // ❷ 각 파일별로 검색
+		findInfos = append(findInfos, FindWordInFile(word, filename))
 	}
 	return findInfos
 }
@@ -55,43 +75,13 @@ func FindWordInFile(word, filename string) FindInfo {
 	defer file.Close()
 
 	lineNo := 1
-	scanner := bufio.NewScanner(file)
-
+	scanner := bufio.NewScanner(file) // ❸ 스캐너를 만듭니다.
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.Contains(line, word) {
+		if strings.Contains(line, word) { // ❹ 한 줄씩 읽으면 단어 포함 여부 검색
 			findInfo.lines = append(findInfo.lines, LineInfo{lineNo, line})
 		}
 		lineNo++
 	}
-
 	return findInfo
-}
-
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("2개 이상의 실행 인수가 필요합니다. ex) ex26.3 word filepath")
-		return
-	}
-
-	word := os.Args[1]
-	files := os.Args[2:]
-
-	findInfos := []FindInfo{}
-
-	for _, path := range files {
-		// 파일찾기
-		findInfos = append(findInfos, FindWordInAllFiles(word, path)...)
-	}
-
-	// 파일 읽기
-	for _, findInfo := range findInfos {
-		fmt.Println(findInfo.filename)
-		fmt.Println("-----")
-		for _, lineInfo := range findInfo.lines {
-			fmt.Println(lineInfo.lineNo, "\t", lineInfo.line)
-		}
-		fmt.Println("-----")
-	}
-
 }
