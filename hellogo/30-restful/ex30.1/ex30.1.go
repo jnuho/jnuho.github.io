64b8c0e8f6713b825a1efb94438014e05b4172c2
jnuho <jnuho@outlook.com>
2022-07-26 17:15:43 +0900

    .

--- hellogo/30-restful/ex30.1/ex30.1.go
+++ hellogo/30-restful/ex30.1/ex30.1.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"sort"
+
+	"github.com/gorilla/mux"
+)
+
+type Student struct {
+	Id int
+	Name string
+	Age int
+	Score int
+}
+
+var students map[int]Student
+var lastId int
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	// list := 
+}
+
+func MakeWebHandler() http.Handler {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/students", GetStudentHandler).Method("GET")
+
+	students = make(map[int]Student)
+	students[1] = Student{1, "aaa", 16, 87}
+	students[2] = Student{2, "bbb", 18, 98}
+
+	lastId = 2
+
+	return mux
+}
+
+func main() {
+
+}
