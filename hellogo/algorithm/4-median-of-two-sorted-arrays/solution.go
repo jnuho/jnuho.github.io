316b6859419ae5e5edfa0cf77edfeec2ba351525
jnuho <jnuho@outlook.com>
2022-08-05 11:59:35 +0900

    .

--- hellogo/algorithm/4-median-of-two-sorted-arrays/solution.go
+++ hellogo/algorithm/4-median-of-two-sorted-arrays/solution.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0
+	nums := append(nums1, nums2...)
+
+	sort.Slice(nums, func(i,j int) bool {
+		return nums[i] < nums[j]
+	})
+	// 1 2 3 4 5
+	// 1 2 3 4
+	length := len(nums)
+	if length %2 == 1 {
+		return float64 (nums[length/2])
+	} else {
+		return float64 (nums[length/2 -1] + nums[length/2])/2.0
+	}
+}
+
+func main() {
+	fmt.Println(findMedianSortedArrays([]int{1,3}, []int{2}))
+	fmt.Println(findMedianSortedArrays([]int{1,2}, []int{3,4}))
 }
