ea715836e33ed2f2d3f0c16a7914269e0fc9ff3b
jnuho <jnuho@outlook.com>
2022-08-10 00:38:13 +0900

    .

---
+++ hellogo/algorithm/8-atoi/solution.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// "123" -> 123
+// "0032" -> 32
+func myAtoi(s string) int {
+	// leading whitespace
+	// - +
+	// num
+
+}
+
+func main() {
+	fmt.Println(myAtoi("42"))
+
+}
