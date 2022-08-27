ff9518b64a856ed606dd6d9a0b4d6dcf0dcdcf41
jnuho <jnuho@outlook.com>
2022-08-27 23:15:10 +0900

    .

--- hellogo/go_coding_test/test.go
+++ hellogo/go_coding_test/test.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
-func main() {
+func dfs(i,j int) bool {
+	
+}
 
+func main() {
+	var N,M int
+	fmt.Scan(&N, &M)
+
+	graph := make([][]int, N)
+	for i:=0; i<N; i++ {
+		graph[i] = make([]int, M)
+		for j:=0; j < M; j++ {
+			fmt.Scan(&graph[i][j])
+		}
+	}
 }
