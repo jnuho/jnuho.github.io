f745c228be02e60e8fba3d5635bcc94dfcd00591
jnuho <jnuho@outlook.com>
2022-08-30 19:01:08 +0900

    .

--- hellogo/go_coding_test/test.go
+++ hellogo/go_coding_test/test.go
@@ -5,7 +5,7 @@ import (
 )
 
 // O(N^2)
-func selection_sort(arr []int) {
+func selection_sort(arr []int) []int {
 	var min_idx int
 	for i:=0; i<len(arr); i++ {
 		min_idx = i
@@ -16,10 +16,11 @@ func selection_sort(arr []int) {
 		}
 		arr[min_idx], arr[i] = arr[i], arr[min_idx]
 	}
+	return arr
 }
 
 // O(N^2)
-func insert_sort(arr []int) {
+func insert_sort(arr []int) []int {
 	for i := 0; i<len(arr); i++ {
 		for j := i; j>=1; j-- {
 			if arr[j] < arr[j-1] {
@@ -27,20 +28,54 @@ func insert_sort(arr []int) {
 			}
 		}
 	}
+	return arr
 }
 
 // O(NlogN)
-func quick_sort(arr []int, ) {
-	pivot := arr[0]
+func quick_sort(arr []int, start, end int) {
+	if start >=end {
+		return
+	}
+	pivot := start
+
+	left := start+1
+	right := end
+
+	for left <= right {
+		for left <= end && arr[left] <= arr[pivot] {
+			left++
+		}
+		for right > start && arr[right] > arr[pivot] {
+			right--
+		}
+
+		if right > left {
+			arr[left], arr[right] = arr[right], arr[left]
+		} else {
+			// 엇갈림 -> for-loop exit
+			arr[right], arr[pivot] = arr[pivot], arr[right]
+		}
+	}
+	quick_sort(arr, start, right-1)
+	quick_sort(arr, right+1, end)
 }
 
 
+func counting_sort(arr []int) []int{
+
+}
+
 func main() {
 	arr := []int {7,5,9,0,3,1,6,2,4,8}
-	selection_sort(arr)
+	fmt.Println(selection_sort(arr))
+
+	arr = []int {7,5,9,0,3,1,6,2,4,8}
+	fmt.Println(insert_sort(arr))
+
+	arr = []int {7,5,9,0,3,1,6,2,4,8}
+	quick_sort(arr, 0, len(arr)-1)
 	fmt.Println(arr)
 
 	arr = []int {7,5,9,0,3,1,6,2,4,8}
-	insert_sort(arr)
-	fmt.Println(arr)
+	fmt.Println(counting_sort(arr))
 }
