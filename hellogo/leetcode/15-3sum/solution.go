c74e377671b7d369a32f353f1126a6153c03a628
jnuho <jnuho@outlook.com>
2022-09-16 17:48:48 +0900

    .

--- hellogo/leetcode/15-3sum/solution.go
+++ hellogo/leetcode/15-3sum/solution.go
@@ -5,8 +5,13 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
-
+	if len(nums) < 3 {
+		return [][]int{}
+	}
+	l := 0
+	r := len(nums)-1
 }
 
 func main() {
+	fmt.Println(threeSum([]int{1,2,3}))
 }
