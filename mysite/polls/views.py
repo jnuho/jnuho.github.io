4ff05877e04b3e7b81085d6c2581e966aa7e4de9
jnuho <cactoos555@gmail.com>
2024-05-23 14:46:45 +0900

    .

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def test(request):
-  return HttpResponse("Hello, world. You're at the test index.")
+    return HttpResponse("Hello, world. You're at the test index.")
