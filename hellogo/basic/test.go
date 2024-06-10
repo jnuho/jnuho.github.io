99991bc17c810fff247214cbee81416ee082158a
jnuho <cactoos555@gmail.com>
2024-06-11 08:16:42 +0900

    .

--- hellogo/basic/test.go
+++ hellogo/basic/test.go
@@ -1,7 +1,20 @@
 package main
 
+import "sync"
+
 func main() {
 	numOfJobs := 10
 	numOfWorkers := 3
+
+	var wg sync.WaitGroup
+	for i := 1; i <= numOfWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 1; j <= numOfJobs; j++ {
+				println("Job", j, "done")
+			}
+		}()
+	}
 
 }
