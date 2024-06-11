921a7a39b1d6f4db28542ec70abace6645c2e317
jnuho <cactoos555@gmail.com>
2024-06-11 13:30:53 +0900

    .

--- hellogo/basic/test.go
+++ hellogo/basic/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 	"sync"
 )
@@ -13,9 +14,36 @@ type counter struct {
 	once sync.Once
 }
 
+func (c *counter) increment() {
+	c.once.Do(func() {
+		c.i = initialValue
+	})
+
+	c.mu.Lock()
+	c.i++
+	c.mu.Unlock()
+}
+
+func (c *counter) display() {
+	fmt.Println(c.i)
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	c := counter{i: 0}          // 카운터 생성
 	done := make(chan struct{}) // 완료 신호 수신용 채널
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			c.increment()
+			done <- struct{}{}
+		}()
+	}
+
+	for i := 0; i < 1000; i++ {
+		<-done
+	}
+
+	c.display()
 }
